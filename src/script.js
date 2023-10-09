2dc6fafdb019b5c13cf6a9d77a59c721174e51b9
Frederick.Laroche <139751013+brightfred@users.noreply.github.com>
2023-10-10 12:50:38 +1300

    dont know what is it yet we are doing

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// Dom Selectors    
+
+const cardContainer = document.getElementById('.card-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addQuestionBtn = document.getElementById('add-question');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
